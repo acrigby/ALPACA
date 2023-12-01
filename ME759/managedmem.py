db5afcaae34905118a6e4eb397d7b420795935f8
acrigby <acrigby@wisc.edu>
2023-12-01 12:11:51 -0600

    Multiprocessing working on ParallelQlearnCPPdequeue

--- ME759/managedmem.py
+++ ME759/managedmem.py
@@ -19,43 +19,34 @@ import glob
 import io
 import base64
 import os
-import torch.multiprocessing as multiprocessing
-from torch.multiprocessing import Process, Manager, Lock, Queue
-from multiprocessing.managers import BaseManager
+import multiprocessing
+from multiprocessing import Process, Manager, Lock, Queue
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 from SolverClasses import *
 import time
 
-SENTINEL = None # no more records sentinel
 semaphore = multiprocessing.Semaphore(1)
 
-class DequeManager(BaseManager):
-    pass
+### Initialize the replay memory
+replay_memory_capacity = 10000   # Replay memory capacity
+replay_mem = ReplayMemory(replay_memory_capacity)   
 
-class DequeProxy(object):
-    def __init__(self, *args):
-        self.deque = collections.deque(*args)
-    def __len__(self):
-        return self.deque.__len__()
-    def appendleft(self, x):
-        self.deque.appendleft(x)
-    def append(self, x):
-        self.deque.append(x)
-    def pop(self):
-        return self.deque.pop()
-    def popleft(self):
-        return self.deque.popleft()
+def forking(episode_collection, max_workers, exploration_profile, plotting_rewards, replay_mem):
+    processes = []
+    for episode in range(max_workers):
+        # Launch the first round of tasks, building a list of ApplyResult objects
+        process = multiprocessing.Process(target = episode_func,args = (episode_collection*max_workers + episode, exploration_profile[episode_collection*max_workers + episode], plotting_rewards, replay_mem))
+        processes.append(process)
+        
+    for p in processes:
+        p.start()
+    
+    for p in processes:
+        p.join()
+    
+    return
 
-# Currently only exposes a subset of deque's methods.
-DequeManager.register('DequeProxy', DequeProxy,
-                      exposed=['__len__', 'append', 'appendleft',
-                               'pop', 'popleft'])
-
-
-process_shared_deque = None  # Global only within each process.
-
-def episode_func(episode_num, tau, plotting_rewards, replay_mem, policy_net, target_net, queue):
-    print(id(policy_net))
+def episode_func(episode_num, tau, plotting_rewards, replay_mem):
     print(f"Process={os.getpid()}")
     print(episode_num, tau)
     # Reset the environment and get the initial state
@@ -80,55 +71,40 @@ def episode_func(episode_num, tau, plotting_rewards, replay_mem, policy_net, tar
         # Apply penalty for bad state
         if terminated or truncated: # if the pole has fallen down 
             next_observation = None
+        
+        semaphore.acquire() 
 
         item = [observation, action, next_observation, reward]
+        #print(item)
 
-        queue.put(item, block = False)
-
-        #print(replay_mem)
         # Update the replay memory
-        semaphore.acquire()
-
         replay_mem.push(observation, action, next_observation, reward)
 
+        observation_list.append(observation)
+        action_list.append(action)
+        next_observation_list.append(next_observation)
+        reward_list.append(reward)
+
+        semaphore.release()
+        
         if score % 8 == 1:
             # Update the network
             if len(replay_mem) > min_samples_for_training: # we enable the training only if we have enough samples in the replay memory, otherwise the training will use the same samples too often
                   update_step(policy_net, target_net, replay_mem, gamma, optimizer, loss_fn, batch_size, device)
+        
+        observation = next_observation
 
-        semaphore.release()
-        observation = next_observation 
+    semaphore.acquire()    
     # Print the final score
     print(f"EPISODE: {episode_num + 1} - FINAL SCORE: {score} - Temperature: {tau}") # Print the final score
-    print(len(replay_mem))
+    
     #print(plotting_rewards)
     plotting_rewards.append(score)
-    queue.put(SENTINEL)
-
-# consume work
-def updater(replay_mem,policy_net, target_net,queue):
-    print('Consumer: Running', flush=True)
-    # consume work
-    while True:
-        print(queue.qsize())
-        # get a unit of work
-        item = queue.get()
-        #print(f'>got {item}', flush=True)
-        observation = item[0]
-        action = item[1]
-        next_observation = item[2]
-        reward = item[3]
-        replay_mem.push(observation, action, next_observation, reward)
-        # check for stop
-        if len(replay_mem) > min_samples_for_training: # we enable the training only if we have enough samples in the replay memory, otherwise the training will use the same samples too often
-            update_step(policy_net, target_net, replay_mem, gamma, optimizer, loss_fn, batch_size, device)
-    
-    print('Consumer: Done', flush=True)
-    return replay_mem,policy_net, target_net
+    semaphore.release()
     
 
-def main(plotting_rewards, queue):
-    global start_time,target_net_update_steps
+def main(plotting_rewards,observation_list, action_list, next_observation_list, reward_list):
+    global start_time,target_net_update_steps,policy_net, target_net
     global gamma, optimizer, loss_fn, batch_size, device, min_samples_for_training
     start_time = time.time()
      
@@ -144,7 +120,6 @@ def main(plotting_rewards, queue):
 
     ### PARAMETERS
     gamma = 0.99   # gamma parameter for the long term reward
-    replay_memory_capacity = 10000   # Replay memory capacity
     #lr = 1e-2   # Optimizer learning rate
     #lr = 1e-4
     lr = 1e-3
@@ -166,20 +141,13 @@ def main(plotting_rewards, queue):
     print(f"STATE SPACE SIZE: {state_space_dim}")
     print(f"ACTION SPACE SIZE: {action_space_dim}")
 
-    ### Initialize the replay memory
-    replay_mem = Bmanager.ReplayMemory(replay_memory_capacity) 
-
-
     ### Initialize the policy network
     policy_net = DQN(state_space_dim, action_space_dim).to(device)
-    policy_net.share_memory()
 
     ### Initialize the target network with the same weights of the policy network
     target_net = DQN(state_space_dim, action_space_dim).to(device)
-    target_net.share_memory()
     target_net.load_state_dict(policy_net.state_dict()) # This will copy the weights of the policy network to the target network
 
-    
     ### Initialize the optimizer
     optimizer = torch.optim.Adam(policy_net.parameters(), lr=lr) # The optimizer will update ONLY the parameters of the policy network
 
@@ -194,63 +162,53 @@ def main(plotting_rewards, queue):
     start_time = time.time()
 
     for episode_collection in range(len(exploration_profile)//max_workers):
-        processes = []
-        seen_sentinel_count = 0
 
-        for episode in range(max_workers):
+        while len(observation_list)>1:
 
-            # Launch the first round of tasks, building a list of ApplyResult objects
-            process = multiprocessing.Process(target = episode_func,args = (episode_collection*max_workers + episode, exploration_profile[episode_collection*max_workers + episode], plotting_rewards, replay_mem,policy_net, target_net, queue))
-            processes.append(process)
-            
-        for p in processes:
-            p.start()
+            shortened_observation_list = observation_list.pop(0)
+            ob_list = []
+            for ob in shortened_observation_list:
+                ob_list.append(ob)
+            observation = np.array(ob_list, dtype=np.float32)
 
-        for p in processes:
-            p.join()
+            action = action_list.pop(0)
 
-
-        while seen_sentinel_count < max_workers:
-            #print(queue.qsize())
-            # get a unit of work
-            item = queue.get(block = False)
-            #print(f'>got {item}', flush=True)
-            if item is SENTINEL:
-                seen_sentinel_count += 1
-            elif item is None:
-                seen_sentinel_count += 1
+            shortened_next_observation_list = next_observation_list.pop(0)
+            next_ob_list = []
+            if shortened_next_observation_list is None:
+                continue
             else:
-                #print(item[0])
-                observation = item[0]
-                action = item[1]
-                next_observation = item[2]
-                reward = item[3]
-                replay_mem.push(observation, action, next_observation, reward)
-                # check for stop
+                for next_ob in shortened_next_observation_list:
+                    next_ob_list.append(next_ob)
+                next_observation = np.array(next_ob_list, dtype=np.float32)
+
+            reward = reward_list.pop(0)
+
+            print([observation, action, next_observation, reward])
+            
+            replay_mem.push(observation, action, next_observation, reward)
+        
+        if len(replay_mem) > min_samples_for_training:
+            update_step(policy_net, target_net, replay_mem, gamma, optimizer, loss_fn, batch_size, device)
+
+        forking(episode_collection, max_workers, exploration_profile, plotting_rewards, replay_mem)
 
         # report
         # all done
-        print('Consumer: Done', flush=True)
-        print("main replay mem")
-        print(len(replay_mem))
-        print("main policy net")
-        print(policy_net)
-        print('Updating target network main')
-        target_net.load_state_dict(policy_net.state_dict()) # This will copy the weights of the policy network to the target network
-        if len(replay_mem) > min_samples_for_training: # we enable the training only if we have enough samples in the replay memory, otherwise the training will use the same samples too often
-            update_step(policy_net, target_net, replay_mem, gamma, optimizer, loss_fn, batch_size, device)
-        print("updating outside")
-        time.sleep(0.5)
+    
+    env.close()
 
 if __name__ == "__main__":
     with Manager() as manager:
         plotting_rewards = manager.list([])
-        queue = manager.Queue() 
 
-        BaseManager.register('ReplayMemory', ReplayMemory)
-        Bmanager = BaseManager()
-        Bmanager.start()
-        main(plotting_rewards, queue)
+        observation_list = manager.list([])
+        action_list = manager.list([])
+        next_observation_list = manager.list([])
+        reward_list = manager.list([])
+
+        main(plotting_rewards,observation_list, action_list, next_observation_list, reward_list)
+
         fig = plt.figure()
         ax = fig.add_subplot()
 
