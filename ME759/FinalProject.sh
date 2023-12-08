add809db814cce887ea57ebae8634a3bf57b2d56
Cole Dunbar <cjdunbar@wisc.edu>
2023-12-08 14:51:37 -0600

    slurm script for running final project

---
+++ ME759/FinalProject.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+#SBATCH -p instruction
+#SBATCH -J pythonTestRun
+#SBATCH -o pythonTestRun.out
+#SBATCH -e pythonTestRun.err
+#SBATCH -t 0-12:00:00
+#SBATCH -c 1
+
+cd $SLURM_SUBMIT_DIR
+
+module load anaconda/full/2021.05
+bootstrap_conda
+conda create --name final_project
+conda activate final_project
+
+source env/bin/activate
+conda install ../gymnasium
+
+python SequentionalQlearnCPP.py
+python ParallelQlearnCPP.py
+
