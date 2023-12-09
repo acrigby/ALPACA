614f07916d10cc5399f696e6a4489a82655c5e17
Cole Dunbar <cjdunbar@wisc.edu>
2023-12-09 12:56:53 -0600

    no cpp run

--- ME759/FinalProjectNoCPP.sh
+++ ME759/FinalProjectNoCPP.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 #SBATCH -p instruction
-#SBATCH -J finalProject
-#SBATCH -o finalProject.out
-#SBATCH -e finalProject.err
+#SBATCH -J finalProjectNoCPP
+#SBATCH -o finalProjectNoCPP.out
+#SBATCH -e finalProfinalProjectNoCPPject.err
 #SBATCH -t 0-12:00:00
 #SBATCH -c 1
 
@@ -10,8 +10,8 @@ cd $SLURM_SUBMIT_DIR
 
 module load anaconda/full/2021.05
 bootstrap_conda
-conda create --name final_project
-conda activate final_project
+conda create --name final_project_nocpp
+conda activate final_project_nocpp
 
 conda install pytorch matplotlib IPython
 
@@ -20,6 +20,6 @@ ls
 pip install ./gymnasium
 cd ME759
 
-python SequentionalQlearnCPP.py
-python ParallelQlearnCPP.py
+python SequentionalQlearn.py
+python ParallelQlearn.py
 
