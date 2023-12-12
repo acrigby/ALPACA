004be153063def4acd8822178fe21bd53e3d2b1a
VSCode Cole <cjdunbar@wisc.edu>
2023-12-12 13:20:34 -0600

    4 ms sleep

--- ME759/rk4.cpp
+++ ME759/rk4.cpp
@@ -48,7 +48,7 @@ void dsdt(double* deriv[], double theta1, double theta2,double dtheta1,double dt
 
 int main(int argc, char *argv[]) {
 
-    usleep(2000);
+    usleep(4000);
 
     double *y = new double[5];
     double *y1 = new double[5];
