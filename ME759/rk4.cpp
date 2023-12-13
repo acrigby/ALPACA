b372a866093f20ca383167bd8a6a9786575b4706
VSCode Cole <cjdunbar@wisc.edu>
2023-12-13 13:40:25 -0600

    removed sleep for ME759 TA's/Dan to run quickly on Euler

--- ME759/rk4.cpp
+++ ME759/rk4.cpp
@@ -48,7 +48,7 @@ void dsdt(double* deriv[], double theta1, double theta2,double dtheta1,double dt
 
 int main(int argc, char *argv[]) {
 
-    usleep(128000);
+    // usleep(128000);
 
     double *y = new double[5];
     double *y1 = new double[5];
